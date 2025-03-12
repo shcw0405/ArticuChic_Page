a01564a8c2079757bc4dd1950140e09f37d43785
cx-shcw <112615230+shaohuacongwen@users.noreply.github.com>
2025-03-13 00:00:13 +0800

    第四次测试

--- ArticuChic/src/router.jsx
+++ ArticuChic/src/router.jsx
@@ -1,33 +1,38 @@
-import { createBrowserRouter } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import News from './pages/News';
-import Members from './pages/Members';
-import Docs from './pages/Docs';
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import News from "./pages/News";
+import Members from "./pages/Members";
+import Docs from "./pages/Docs";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "news",
+          element: <News />,
+        },
+        {
+          path: "members",
+          element: <Members />,
+        },
+        {
+          path: "docs",
+          element: <Docs />,
+        },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: 'news',
-        element: <News />,
-      },
-      {
-        path: 'members',
-        element: <Members />,
-      },
-      {
-        path: 'docs',
-        element: <Docs />,
-      },
-    ],
-  },
-]);
+    basename: "/ArticuChic_Page",
+  }
+);
 
 export default router;
