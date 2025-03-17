c6ae3a3b740de626123d95ab0af847cb3022ed02
Violesa <126861225+Violesa@users.noreply.github.com>
2025-03-17 15:24:38 +0800

    Update index.js

--- news/authors/wbr/index.js
+++ news/authors/wbr/index.js
@@ -1,1 +1,24 @@
 
+
+
+import WuBoRui_report_week2 from "./WuBoRui_report_week2";
+
+// 管理员的所有新闻条目
+const adminNews = {
+  "WuBoRui_report_week2": {
+    component: WuBoRui_report_week2,
+    info: {
+      id: "WuBoRui_report_week2",
+      title: "BoRui’s work from 3.11 to 3.16",
+      date: "2025-03-17",
+      summary: "I complished the request analysis in this week",
+      image: new URL("../../../ArticuChic/assets/vio.jpg", import.meta.url)
+        .href,
+      category: "Weekly Work",
+      author: "BoRui",
+    },
+  },
+  // 添加更多新闻
+};
+
+export default adminNews;
