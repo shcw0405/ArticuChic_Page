9a898a74bf36d0444812493ebdb87979df3fdffd
zzyuanyi <2649157045@qq.com>
2025-03-19 14:13:59 +0800

    250319

--- news/authors/zyw/zywNews1.jsx
+++ news/authors/zyw/zywNews1.jsx
@@ -28,49 +28,100 @@ const NewsTemplate = () => {
     },
   ];
 
-  return (
-    <div className="news-container">
-      <h1>{title}</h1>
-      <div className="news-meta">
-        <span>Publication Date: {date}</span>
-        <span>Author: {author}</span>
-      </div>
+  const content = [
+    { type: "text", value: "This week, the task I chose was to ask large models, including but not limited to DeepSeek, DouBao, TongyiQianwen, and Kimi, to divide responsibilities. I received multiple division plans and finally summarized and selected the plan provided by DeepSeek." },
+     {
+     type: "image",
+     url: new URL("../../../ArticuChic/assets/zyw/p1.png", import.meta.url).href,
 
-      <div className="news-content">
-        {/* Write News here */}
-        <p>Content of News...</p>
+     alt: "",
+     caption: "",
+     },
 
-        <div className="news-image">
-          <img
-            src={new URL("../../../ArticuChic/assets/test.jpg", import.meta.url).href}
-            alt="Project Launch"
-          />
-          <p className="image-caption">
-            If you need to include images, please upload the image files to the
-            ArticuChic/assets directory and insert them into the content
-            following the specified format.
-          </p>
-        </div>
 
-        {/* File List for Download */}
-        {downloads.length > 0 && (
-          <div className="downloads-section">
-            <h3>Related Files Download</h3>
-            <ul>
-              {downloads.map((file, index) => (
-                <li key={index}>
-                  <a href={file.url} download>
-                    {file.name}
-                  </a>
-                  <p>{file.description}</p>
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </div>
+   {
+     type: "image",
+     url: new URL("../../../ArticuChic/assets/zyw/p2.png", import.meta.url)
+       .href,
+     alt: "",
+     caption: "",
+   },
+
+   
+   {
+     type: "image",
+     url: new URL("../../../ArticuChic/assets/zyw/p3.png", import.meta.url)
+       .href,
+     alt: "",
+     caption: "",
+   },
+   {
+    type: "text",
+    value:
+      "follows are summary",
+  },
+   {
+     type: "image",
+     url: new URL("../../../ArticuChic/assets/zyw/p4.png", import.meta.url)
+       .href,
+     alt: "",
+     caption: "",
+   },
+   {
+     type: "image",
+     url: new URL("../../../ArticuChic/assets/zyw/p5.png", import.meta.url)
+       .href,
+     alt: "",
+     caption: "",
+   },
+  
+  
+ ];
+ return (
+  <div className="news-container">
+    <h1>{title}</h1>
+    <div className="news-meta">
+      <span>Publication Date: {date}</span>
+      <span>Author: {author}</span>
     </div>
-  );
+
+    <div className="news-content">
+      {/* 动态渲染文本和图片 */}
+      {content.map((item, index) => {
+        if (item.type === "text") {
+          return <p key={index}>{item.value}</p>;
+        } else if (item.type === "image") {
+          return (
+            <div key={index} className="news-image">
+              <img src={item.url} alt={item.alt} />
+              {item.caption && (
+                <p className="image-caption">{item.caption}</p>
+              )}
+            </div>
+          );
+        }
+        return null;
+      })}
+
+      {/* File List for Download */}
+      {downloads.length > 0 && (
+        <div className="">
+          <h3>Related Files Download</h3>
+          <ul>
+            {downloads.map((file, index) => (
+              <li key={index}>
+                <a href={file.url} download>
+                  {file.name}
+                </a>
+                <p>{file.description}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  </div>
+);
 };
 
 export default NewsTemplate;
